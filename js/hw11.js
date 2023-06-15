3b8cfb74e7cff68ad626b13a98a397972af32790
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-06-15 18:08:34 +0300

    hw 12

--- js/hw11.js
+++ js/hw11.js
@@ -143,33 +143,33 @@
 
 // завдання 7
 
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-const isLoginValid = function (login) {
-    return login.length >= 4 && login.length <= 16;
-};
+// const isLoginValid = function (login) {
+//     return login.length >= 4 && login.length <= 16;
+// };
 
-const isLoginUnique = function (allLogins, login) {
-    return !allLogins.includes(login);
-};
+// const isLoginUnique = function (allLogins, login) {
+//     return !allLogins.includes(login);
+// };
 
-const addLogin = function (allLogins, login) {
-    if(!isLoginValid(login)){
-        return ("Помилка! Логін повинен бути від 4 до 16 символів");
-    }
+// const addLogin = function (allLogins, login) {
+//     if(!isLoginValid(login)){
+//         return ("Помилка! Логін повинен бути від 4 до 16 символів");
+//     }
 
-    if(!isLoginUnique(allLogins, login)){
-        return ("Такий логін вже використовується!")
-    }
+//     if(!isLoginUnique(allLogins, login)){
+//         return ("Такий логін вже використовується!")
+//     }
 
-    allLogins.push(login);
-    return ("Логін успішно доданий!")
-};
+//     allLogins.push(login);
+//     return ("Логін успішно доданий!")
+// };
 
-/*
- * Виклич функції для перевірки працездатності твоєї реалізації.
- */
-console.log(addLogin(logins, 'Ajax')); // 'Логін успішно доданий!'
-console.log(addLogin(logins, 'robotGoogles')); // 'Такий логін вже використовується!'
-console.log(addLogin(logins, 'Zod')); // 'Помилка! Логін повинен бути від 4 до 16 символів'
-console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Помилка! Логін повинен бути від 4 до 16 символів'
+// /*
+//  * Виклич функції для перевірки працездатності твоєї реалізації.
+//  */
+// console.log(addLogin(logins, 'Ajax')); // 'Логін успішно доданий!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такий логін вже використовується!'
+// console.log(addLogin(logins, 'Zod')); // 'Помилка! Логін повинен бути від 4 до 16 символів'
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Помилка! Логін повинен бути від 4 до 16 символів'
---
+++ js/hw12.js
@@ -0,0 +1,117 @@
+// // завдання 1
+
+// function makePizza() {
+
+//     return "Your pizza is being prepared, please wait.";
+  
+//   }
+  
+//   // Change code below this line
+  
+//   const result = makePizza();
+  
+//   const pointer = makePizza;
+//   console.log(result);
+//   console.log(pointer());
+
+// завдання 2
+
+// function deliverPizza(pizzaName) {
+
+//     return `Delivering ${pizzaName} pizza.`;
+
+//   }
+
+//   function makePizza(pizzaName) {
+
+//     return `Pizza ${pizzaName} is being prepared, please wait...`;
+
+//   }
+
+//   // Chande code below this line
+
+//   function makeMessage(pizzaName, callback) {
+
+//     return callback(pizzaName);
+
+//   }
+
+//   console.log(makeMessage("peperoni", makePizza));
+//   console.log(makeMessage("peperoni", deliverPizza));
+// завдання 3
+
+// function makePizza(pizzaName, callback) {
+
+//     console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+
+//     callback(pizzaName);
+
+//   }
+
+//   makePizza("Royal Grand", function deliverPizza(pizzaName) {
+
+//     console.log(`Delivering pizza ${pizzaName}.`);
+
+//   });
+
+//   // Change code below this line
+
+//   makePizza("Ultracheese", function eatPizza(pizzaName){
+//     console.log(`Eating pizza ${pizzaName}`);
+//   });
+
+// завдання 4
+
+// const btnref = document.querySelector("[data-btn]");
+
+// function showInfo() {
+//   console.log("Hello World!");
+// }
+
+// btnref.addEventListener("click", showInfo);
+// // завдання 5
+
+// const buttonref = document.querySelector("[data-button]");
+// let click = 0;
+
+// function buttonClickNum() {
+//   click += 1;
+//   console.log(click);
+// }
+
+// buttonref.addEventListener("click", buttonClickNum);
+
+
+// завдання 6
+// const applyCallbackToEachElement = function (arr, callback) {
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//      result.push(callback(arr[i]));  
+//   }
+//   return result
+// }
+
+// const squareCallback = function(arr){
+//   return arr * arr;
+// }; 
+
+
+// const arr = [1, 2, 3, 4, 5];
+
+
+// const result = applyCallbackToEachElement(arr, squareCallback);
+
+// console.log(result); // [1, 4, 9, 16, 25]
+// завдання 7
+
+// const calculateDiscountedPrice = function (price, discount, callback) {
+
+   
+
+// };
+
+
+
+// const showDiscountedPrice = ;
+
+// calculateDiscountedPrice(100, 10, showDiscountedPrice); // Discounted price: 90
