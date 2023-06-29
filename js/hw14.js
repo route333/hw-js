2f10f2bee3e7fc9cfa66c20a8dd505ecb541c898
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-06-29 16:55:41 +0300

    hw 14

---
+++ js/hw14.js
@@ -0,0 +1,137 @@
+// завдання 1
+// const myArray = {
+//     name: "Oleh",
+//     age: 14,
+//     city: "Lviv",
+//     occupation: "no",
+// }
+
+// myArray.age = 15;
+
+// console.log(myArray);
+// console.log(myArray.arr);
+
+// завдання 2
+
+// const basket = {
+//   goods: [
+//     {
+//       name: "water",
+//       price: 10,
+//       availability: true,
+//       inStock: true,
+//       calories: null,
+//     },
+//     {
+//       name: "apple",
+//       price: 22,
+//       availability: false,
+//       inStock: undefined,
+//       calories: 52,
+//     },
+//     {
+//       name: "ice cream",
+//       price: 25,
+//       availability: true,
+//       inStock: true,
+//       calories: 207,
+//     },
+//     {
+//       name: "coke zero",
+//       price: 16,
+//       availability: true,
+//       inStock: undefined,
+//       calories: null,
+//     },
+//   ],
+// };
+
+// console.log(basket);
+// завдання 3
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// let aptRating = apartment.rating;
+
+// let aptDescr = apartment.descr;
+
+// let aptPrice = apartment.price;
+
+// let aptTags = apartment.tags;
+
+// let ownerName = apartment.owner.name;
+
+// let ownerPhone = apartment.owner.phone;
+
+// let ownerEmail = apartment.owner.email;
+
+// let numberOfTags = apartment.tags.length;
+
+// let firstTag = apartment.tags[0];
+
+// let lastTag = apartment.tags[apartment.tags.length - 1];
+
+// aptRating = apartment["rating"];
+
+// aptDescr = apartment["descr"];
+
+// aptPrice = apartment["price"];
+
+// aptTags = apartment["tags"];
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Henry Sibola";
+// apartment.tags.push("trusted");
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: "Jamaica",
+//   city: "Kingston",
+// };
+
+// console.log(apartment);
+// завдання 4
+
+// const name = "Repair Droid";
+
+// const price = 2500;
+
+// const image = "https://via.placeholder.com/640x480";
+
+// const tags = ["on sale", "trending", "best buy"];
+
+ 
+
+// const product = {
+//     name,
+//     price,
+//     image,
+//     tags,
+// };
+
+// console.log(product);
+
+// завдання 5
+
+// const emailInputName = "email";
+
+// const passwordInputName = "password";
+
+// const credentials = {
+//     email: [emailInputName],
+//     password: [passwordInputName],
+// };
+// console.log(credentials);
