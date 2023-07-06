0dae42db3d05d379b64a4f4a26c58069c373ffa3
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-07-06 16:05:05 +0300

    hw 15 without 3

---
+++ js/hw15.js
@@ -0,0 +1,77 @@
+// завдання 1
+// const user = {
+//     hobby: "aaa",
+//     premium: true,
+// }
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+// for(const i of Object.keys(user)){
+//     console.log(`${i}: ${user[user]}`)
+// }
+// завдання 2
+// const countProps = obj => Object.keys(obj).length;
+
+// console.log(countProps(user));
+// завдання 3
+const findBestEmployee = function (employees) {};
+
+// завдання 4
+
+// function countTotalSalary(employees){
+//     let totalSalary = 0
+
+//     for(const employee in employees){
+//         totalSalary += employees[employee]
+//     }
+//     return totalSalary;
+// }
+// const workers = {
+//     John: 1000,
+//     Anna: 1500,
+//     Peter: 1200,
+//     Mary: 2000
+//   };
+
+//   console.log(countTotalSalary(workers));
+
+// завдання 5
+// function getAllPropValues(arr, prop) {
+//   const propValues = [];
+//   for (const obj of arr) {
+//     if (prop in obj) {
+//       propValues.push(obj[prop]);
+//     }
+//   }
+//   return propValues;
+// }
+
+// const products = [
+//   { name: "Apple", price: 10 },
+//   { name: "Apple", price: 10 },
+//   { name: "Apple", price: 10 },
+//   { name: "Apple", price: 10 },
+// ];
+
+// console.log(getAllPropValues(products, `price`));
+
+// завдання 6
+
+// function calculateTotalPrice(allProdcuts, productName){
+//     let totalPrice = 0;
+//     for(const product of allProdcuts){
+//         if(product.name === productName){
+//             totalPrice = product.price * product.quantity;
+//         }
+//     }
+//     return totalPrice;
+// }
+
+// const products = [
+//     { name: 'milk', price: 30, quantity: 10 },
+//     { name: 'yogurt', price: 20, quantity: 5 },
+//     { name: 'chips', price: 50, quantity: 8 },
+//     { name: 'cola', price: 20, quantity: 3 }
+//   ];
+
+// console.log(calculateTotalPrice(products, 'chips'));
