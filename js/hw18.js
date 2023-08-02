e3164b726411cf3994e29b7321c9e86910c63de0
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-08-03 00:31:37 +0300

    hw18

---
+++ js/hw18.js
@@ -0,0 +1,54 @@
+// const users = [
+//     {
+//       id: "user1",
+//       name: "Oleh",
+//       gender: "male",
+//       eyeColor: "green",
+//       isActive: true,
+//       email: "oleh@gmail.com",
+//       age: 14,
+//     },
+//     {
+//       id: "user2",
+//       name: "Max",
+//       gender: "male",
+//       eyeColor: "brown",
+//       isActive: false,
+//       email: "max@gmail.com",
+//       age: 16,
+//     },
+//     {
+//       id: "user3",
+//       name: "Ann",
+//       gender: "female",
+//       eyeColor: "green",
+//       isActive: false,
+//       email: "ann@gmail.com",
+//       age: 15,
+//     },
+//   ];
+
+
+// //   завдання1
+//   const usersName = users.map((users) => users.name);
+//   console.log(usersName);
+
+// // завдання2
+//   const usersEyeColor = users.map((users) => users.eyeColor);
+//   console.log(usersEyeColor); 
+
+// // завдання3
+//   const usersGender = users.map((users) => users.name + " is " + users.gender);
+//   console.log(usersGender)
+
+// // завдання4
+//   const usersIsActive = users.filter(users =>(users.isActive === false));
+//   console.log(usersIsActive);
+
+// // завдання5
+//   const usersEmail = users.reduce((emails, user) => emails + " " +user.email, "");
+//   console.log(usersEmail);
+  
+// // завдання6
+//   const usersAge = users.sort((a, b) => a.age - b.age);
+//   console.log(usersAge)
