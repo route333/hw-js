f34886231edc69ff7ce33c3607e9ba74e827093c
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-08-10 14:51:25 +0300

    hw 19

---
+++ js/hw19.js
@@ -0,0 +1,27 @@
+// завдання1
+// const array2 = [
+//   "apple",
+//   "banana",
+//   "orange",
+//   "banana",
+//   "grape",
+//   "apple",
+//   "kiwi",
+// ];
+
+// const result = array2.map((ar) => ar[0] + ar[ar.length - 1]);
+// console.log(result);
+
+// завдання 2
+// const people = [
+//   { name: "John", surname: `Doe`, age: 32, occupation: "programmer" },
+//   { name: "Jane", surname: `Lee`, age: 26, occupation: "teacher" },
+//   { name: "Mike", surname: `Watson`, age: 42, occupation: "engineer" },
+//   { name: "Emily", surname: `Brad`, age: 29, occupation: "designer" },
+// ];
+
+// const result = people.map((man, id) => ({
+//   id: id + 1,
+//   fullName: `${man.name} ${man.surname}`,
+// }));
+// console.log(result);
